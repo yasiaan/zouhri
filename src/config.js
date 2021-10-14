d7801a96c24e8f1d44f88bc24104351b71c585d3
yasiaan <yzouhri5@gmail.com>
2021-10-14 15:41:27 +0100

    commit

---
+++ src/config.js
@@ -0,0 +1,130 @@
+// config.js
+module.exports = {
+    github: {
+        username: 'yasiaan', // Your GitHub org/user name. (Required)
+        sortBy: 'stars', // stars | updated
+        limit: 8, // How many projects to display.
+        exclude: {
+            forks: false, // Forked projects will not be displayed if set to true.
+            projects: [] // These projects will not be displayed. example: ['my-project1', 'my-project2']
+        }
+    },
+    social: {
+        linkedin: 'ariful-alam',
+        twitter: 'arif_swozon',
+        facebook: '',
+        dribbble: '',
+        behance: '',
+        medium: '',
+        devto: '',
+        website: 'https://arifszn.github.io',
+        phone: '',
+        email: 'contact@arifszn.com'
+    },
+    skills: [
+        'PHP',
+        'Laravel',
+        'JavaScript',
+        'React.js',
+        'Vue.js',
+        'Node.js',
+        'Jquery',
+        'MySQL',
+        'Git',
+        'Docker',
+        'CSS',
+        'Antd',
+        'Tailwind',
+        'Bootstrap',
+    ],
+    experiences: [
+        { 
+            company: 'Monstarlab Bangladesh',
+            position: 'Backend Engineer',
+            from: 'September 2021',
+            to: 'Present'
+        },
+        { 
+            company: 'Orangetoolz - My Offer 360 Degree',
+            position: 'Jr. Full Stack Engineer',
+            from: 'July 2019',
+            to: 'August 2021'
+        },
+        { 
+            company: 'Techvillage',
+            position: 'Jr. Software Engineer',
+            from: 'January 2019',
+            to: ' June 2019'
+        }
+    ],
+    education: [
+        { 
+            institution: 'Cycle d’ingénieur à l’Institut National de Statistique et d’Économie Appliquée, Rabat',
+            degree: 'Bachelor of Science',
+            from: '2015',
+            to: '2019'
+        },
+        { 
+            institution: 'Cantonment College, Jessore',
+            degree: 'Higher Secondary Certificate (HSC)',
+            from: '2012',
+            to: '2014',
+        },
+        { 
+            institution: 'Chowgacha Shahadat Pilot High School',
+            degree: 'Secondary School Certificate (SSC)',
+            from: '2007',
+            to: '2012'
+        }
+    ],
+    blog: {
+        // Display blog posts from your medium or dev.to account. (Optional)
+        source: 'dev.to', // medium | dev.to
+        username: 'arifszn',
+        limit: 2 // How many posts to display. Max is 10.
+    },
+    googleAnalytics: {
+        // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+        id: '' // Please remove this and use your own tag id
+    },
+    hotjar: {
+        id: '', //  Please remove this and use your own id
+        snippetVersion : 6
+    },
+    themeConfig: {
+        default: 'cupcake',
+
+        // Hides the switch in the navbar
+        // Useful if you want to support a single color mode
+        disableSwitch: false,
+
+        // Should we use the prefers-color-scheme media-query,
+        // using user system preferences, instead of the hardcoded default
+        respectPrefersColorScheme: true,
+
+        // Available themes. To remove any theme, exclude from here.
+        themes: [
+            'light',
+            'dark',
+            'cupcake',
+            'bumblebee',
+            'emerald',
+            'corporate',
+            'synthwave',
+            'retro',
+            'cyberpunk',
+            'valentine',
+            'halloween',
+            'garden',
+            'forest',
+            'aqua',
+            'lofi',
+            'pastel',
+            'fantasy',
+            'wireframe',
+            'black',
+            'luxury',
+            'dracula'
+        ]
+    }
+}
